f58ceef24fbcbeae4403320eaf4a3c6b3c7bd331
Dheeraj Jha <jhadheeraj97@gmail.com>
2025-08-24 11:49:56 +0000

    मैने कहा ये badge scroll hona chahiye, lekin sare days ek line me visibl

--- src/components/ui/tabs.tsx
+++ src/components/ui/tabs.tsx
@@ -15,7 +15,7 @@ const TabsList = React.forwardRef<
   <TabsPrimitive.List
     ref={ref}
     className={cn(
-      "inline-flex h-auto items-center rounded-md bg-transparent p-1 text-muted-foreground gap-2",
+      "flex h-auto items-center rounded-md bg-transparent p-1 text-muted-foreground gap-2",
       className
     )}
     {...props}
